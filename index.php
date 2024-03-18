0d16b4dcf96968e478bde78ca65acdbe35d87af0
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-03-18 16:09:39 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="form.php">Cadastrar</a><br>
+    <table>
+         <thead>
+        <tr>
+            <th>Id</th>
+            <th>Email</th>
+            <th>Opções</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+        <td>1</td>
+        <td>Thiago</td>
+        <td>Alterar</td>
+        <td>Excluir</td>
+</tr>
+</tbody>
+</table>
+
+   Óla mundo
+   <form method="post">
+
+</body>
+</form>
+</html>
