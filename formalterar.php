590920f0200070cd394311d75b5bf86d26bbc05e
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-03-18 16:39:32 -0300

    alterar feitcho

--- formalterar.php
+++ formalterar.php
@@ -6,6 +6,11 @@
     <title> Alterar Usuario </title>
 </head>
 <body>
-    
+<form action="alterar.php" method="post">
+        ID <input type="number" name="id_usuario" value="<?= $usuario['id_usuario'] ?>"><br>
+        Email <input type="email" name="email" value="<?= $usuario['email'] ?>"><br>
+        Senha <input type="password" name="senha" value="<?= $usuario['senha'] ?>"><br>
+        <input type="submit" value="Salvar"><br>
+    </form>
 </body>
 </html>
